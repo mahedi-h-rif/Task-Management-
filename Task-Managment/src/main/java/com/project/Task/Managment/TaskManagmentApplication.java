934187b572f517263de0ee70178b8c0aa7e444e0
mahedi-h-rif <136273197+mahedi-h-rif@users.noreply.github.com>
2024-01-31 13:06:55 +0600

    Initial setup and jwt configuration

---
+++ Task-Managment/src/main/java/com/project/Task/Managment/TaskManagmentApplication.java
@@ -0,0 +1,13 @@
+package com.project.Task.Managment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskManagmentApplication.class, args);
+	}
+
+}
---
+++ Task-Managment/src/test/java/com/project/Task/Managment/TaskManagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.Task.Managment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
